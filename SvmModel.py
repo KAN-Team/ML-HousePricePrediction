a4a9ce29652ca3495a8cf045e0058a539ff97fc0
Abanoub-Asaad <abanob2018170001@cis.asu.edu.eg>
2021-12-28 01:58:52 +0200

    feat: add svm model

--- SvmModel.py
+++ SvmModel.py
@@ -1,7 +1,31 @@
+import pandas as pd
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
+# load the data
+data = pd.read_csv('House_Data_Classification.csv')
+print(data)
+print(data['PriceRate'])
 
-#model = SVC()
-#model.fit(x_train, y_train)
+X = data.iloc[:, 0:5]     # Features
+Y = data['PriceRate']     # Label
 
-#model.score(x_test, y_test)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
+
+# generate the model
+cls_model = SVC(kernel="linear")
+
+# train the model
+cls_model.fit(X_train, y_train)
+
+# predict the response
+predictions = cls_model.predict(X_test)
+print("accuracy:", metrics.accuracy_score(y_test, y_pred=predictions))
+
+# precision score
+print("precision:", metrics.precision_score(y_test, y_pred=predictions))
+
+# recall score
+print("recall:", metrics.recall_score(y_test, y_pred=predictions))
+print(metrics.classification_report(y_test, y_pred=predictions))
